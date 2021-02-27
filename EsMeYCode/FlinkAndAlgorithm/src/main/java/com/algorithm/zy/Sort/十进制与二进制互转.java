6bdd2b18fdf5166809f6de28a02982132f7c24fa
EsMe <32116495+EssMee@users.noreply.github.com>
2021-02-27 17:29:34 +0800

    Create 十进制与二进制互转.java

---
+++ EsMeYCode/FlinkAndAlgorithm/src/main/java/com/algorithm/zy/Sort/十进制与二进制互转.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+//        int asc = 'Z';
+//        System.out.println("ASCII: " + asc);
+//        System.out.println(4999 + 1000 - 4999 % 1000);
+        int test = 50;
+        String binary = "110010";
+        System.out.println("原数：" + test);
+        System.out.println("十进制整数转二进制字符串：" + Integer.toBinaryString(test));
+        System.out.println("二进制字符串转十进制整数：" + Integer.valueOf(binary, 2));
+    }
+}

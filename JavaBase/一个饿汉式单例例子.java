bb257d0fcf2dad359bf5e34b54ee9975da169dc9
EsMe <32116495+EssMee@users.noreply.github.com>
2021-03-13 23:23:57 +0800

    Create 一个饿汉式单例例子.java

---
+++ JavaBase/一个饿汉式单例例子.java
@@ -0,0 +1,40 @@
+package com.algorithm.zy.msb;
+
+import java.util.concurrent.TimeUnit;
+
+/*懒汉模式： 有人调用的时候才会new一个INSTANCE出来
+* 线程不安全，肯定不能保证只new同一个对象。
+* 可以打印一下每个对象的hashCode。
+* */
+public class SingleTonMode2 {
+    private static SingleTonMode2 INSTANCE;
+
+    private SingleTonMode2() {
+    }
+
+    /*如果这个方法加了synchronized，那100次new出来的对象肯定是同一个。*/
+    public synchronized static SingleTonMode2 getInstance() {
+        if (INSTANCE == null) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            INSTANCE = new SingleTonMode2();
+        }
+        return INSTANCE;
+    }
+
+    public void m() {
+        System.out.println("mmmmmmmmmmmmmm");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                System.out.println(SingleTonMode2.getInstance().hashCode());
+            }).start();
+        }
+    }
+}
+

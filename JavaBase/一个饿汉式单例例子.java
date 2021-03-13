17220bbf9886045f467a7c70915e7d2596c5b24a
EsMe <32116495+EssMee@users.noreply.github.com>
2021-03-13 23:28:27 +0800

    Update 一个饿汉式单例例子.java

--- JavaBase/一个饿汉式单例例子.java
+++ JavaBase/一个饿汉式单例例子.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 /*懒汉模式： 有人调用的时候才会new一个INSTANCE出来
 * 线程不安全，肯定不能保证只new同一个对象。
 * 可以打印一下每个对象的hashCode。
+* 缺点是synchronized锁住了整个getInstance方法，如果需要在里面有业务代码时，业务代码也会被锁住。
 * */
 public class SingleTonMode2 {
     private static SingleTonMode2 INSTANCE;

ceee980681f458dd468c19703d2d360744629915
EsMe <32116495+EssMee@users.noreply.github.com>
2021-03-23 23:00:22 +0800

    Add files via upload

---
+++ JavaBase/msb/Synchro/FairAndUnFairTest.java
@@ -0,0 +1,78 @@
+package com.algorithm.zy.msb.synchro;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairAndUnFairTest {
+    private static Lock failLock = new ReentrantLock2(true);
+    private static Lock unfailLock = new ReentrantLock2(false);
+    private static CountDownLatch start;
+
+    @Test
+    public void fair() {
+        testLock(failLock);
+    }
+
+    @Test
+    public void unfair() {
+        testLock(unfailLock);
+    }
+
+    private void testLock(Lock lock) {
+        start = new CountDownLatch(1);
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Job(lock);
+            thread.setName("" + i);
+            thread.start();
+        }
+        start.countDown();
+    }
+    private static class ReentrantLock2 extends ReentrantLock {
+        ReentrantLock2(boolean fair) {
+            super(fair);
+        }
+
+        @Override
+        protected Collection<Thread> getQueuedThreads() {
+            List<Thread> arrayList = new ArrayList<>(super.getQueuedThreads());
+            Collections.reverse(arrayList);
+            return arrayList;
+        }
+    }
+
+    private class Job extends Thread {
+        private Lock lock;
+        public Job(Lock lock) {
+            this.lock = lock;
+        }
+
+        @Override
+        public void run() {
+            try {
+                start.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < 2; i++) {
+                lock.lock();
+                try {
+                    System.out.println("Lock by [" + getName() + "], Waiting by : " + ((ReentrantLock2) lock).getQueuedThreads());
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return getName();
+        }
+    }
+}
---
+++ JavaBase/msb/Synchro/TwinsLockTest.java
@@ -0,0 +1,40 @@
+import com.algorithm.zy.msb.synchro.TwinsLock;
+
+import java.util.concurrent.locks.Lock;
+
+public class TwinsLockTest {
+    @Test
+    public void test() {
+        final Lock lock = new TwinsLock();
+
+        class Worker extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println(Thread.currentThread().getName());
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            Worker w = new Worker();
+            w.setDaemon(true);
+            w.start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
